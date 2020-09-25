5bba17acef77b8d9b1f7744f7ac52f02441196cf
Riky Perdana <perdanariky@gmail.com>
2020-09-25 23:09:30 +0700

    new playlist

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ menus = {
   registration: {
     full: 'Pendaftaran', icon: 'address-book',
     desc: 'Adalah menu yang dapat digunakan oleh user bidang pendaftaran untuk mencari data pasien lama, menginput data pasien baru, meregistrasikan seorang pasien ke poliklinik atau ke instalasi IGD. Tersedia modal cetak laporan untuk admin.',
-    video: 'https://www.youtube.com/watch?v=irSxnKSRIOI&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ',
+    video: 'https://www.youtube.com/watch?v=EDqkltXaILY&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=1&t=3s',
     children: {
       icd: {full: 'Kodifikasi', icon: 'code'},
       queue: {full: 'Antrian', icon: 'stream'}
@@ -12,17 +12,17 @@ menus = {
   emergency: {
     full: 'IGD', icon: 'heartbeat',
     desc: 'Adalah menu yang dapat digunakan oleh perawat dan dokter untuk menangani pasien yang telah didaftarkan sebelumnya oleh petugas pendaftaran. Tersedia form SOAP bagi perawat dan dokter untuk mencatat asuhan dan rekam medisnya.',
-    video: 'https://www.youtube.com/watch?v=sgXKz5Y9kks&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=4'
+    video: 'https://www.youtube.com/watch?v=a7X9jwGdlHI&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=3&t=2s'
   },
   outpatient: {
     full: 'Rawat Jalan', icon: 'walking',
     desc: 'Adalah menu yang dapat digunakan oleh perawat dan dokter untuk menangani pasien yang terdaftar pada poliklinik dimana petugas medis tersebut ditempatkan. Tersedia antrian dan halaman rekam medis untuk mencatatkan informasi SOAP pasien.',
-    video: 'https://www.youtube.com/watch?v=O63JFK5j_Mk&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=3'
+    video: 'https://www.youtube.com/watch?v=bLTxJLxBL1g&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=2&t=1s'
   },
   inpatient: {
     full: 'Rawat Inap', icon: 'bed',
     desc: 'Adalah menu yang dapat digunakan oleh perawat dan dokter untuk mengadmisi seorang pasien ke rawat inap dan menentukan bed bagi pasien. Tersedia form SOAP per kali observasi bagi pasien yang diinapkan. Sudah terintegrasi dengan menu daftar ketersediaan bed, jadwal operasi, gizi, dan BPJS.',
-    video: 'https://www.youtube.com/watch?v=30ePQptZ7CA&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=5',
+    video: 'https://www.youtube.com/watch?v=ZMCLcKx6q6A&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=4&t=1s',
     children: {
       beds: {full: 'Daftar Kamar', icon: 'bed'},
       surgery: {full: 'Antrian Bedah', icon: 'procedures'},
@@ -36,7 +36,7 @@ menus = {
   storage: {
     full: 'Storage', icon: 'cubes',
     desc: 'Adalah menu yang dapat diakses oleh petugas gudang, apoteker, perawat dan dokter untuk mengelola atau sekedar melihat ketersediaan stok obat tertentu yang tercatat oleh sistem. Terdapat menu amprah yang dapat digunakan antara instalasi farmasi dan seluruh instalasi lainnya untuk pemutasian barang, berikut dengan cetak laporan mutasinya.',
-    video: 'https://www.youtube.com/watch?v=StlCB8HqFU4&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=2',
+    video: 'https://www.youtube.com/watch?v=ufjDfjjr9IA&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=5&t=122s',
     children: {
       transfer: {full: 'Amprah', icon: 'exchange-alt'}
     }
@@ -44,20 +44,22 @@ menus = {
   pharmacy: {
     full: 'Apotik', icon: 'pills',
     desc: 'Adalah menu khusus bagi apoteker untuk menyerahkan obat yang diresepkan oleh dokter dari instalasi rawat jalan, IGD, dan rawat inap. Juga disediakan menu untuk penjualan obat bebas bagi non-pasien.',
-    video: 'https://www.youtube.com/watch?v=StlCB8HqFU4&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=2' // sama dengan farmasi
+    video: 'https://www.youtube.com/watch?v=ufjDfjjr9IA&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=5&t=122s'
   },
   laboratory: {
     full: 'Laboratorium', icon: 'flask',
     desc: 'Adalah menu khusus bagi laborat yang akan menerima permintaan uji klinis oleh dokter dan mengisikan respon hasil diagnosanya. Secara otomatis hasil tersebut akan muncul pada cetak laporan SOAP dokter.',
+    video: 'https://www.youtube.com/watch?v=XKkdONSGOtE&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=6&t=1s'
   },
   radiology: {
     full: 'Radiologi', icon: 'radiation',
     desc: 'Adalah menu khusus bagi radiologist yang akan menerima permintaan uji klinis oleh dokter dan mengisikan respon hasil diagnosanya. Secara otomatis hasil tersebut akan muncul pada cetak laporan SOAP dokter.',
+    video: 'https://www.youtube.com/watch?v=fqIq6S-ba3U&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=7&t=1s'
   },
   management: {
     full: 'Manajemen', icon: 'users',
     desc: 'Adalah menu khusus bagi administrator sistem untuk mengelola hak akses bagi seluruh karyawan yang diizinkan untuk masuk ke dalam sistem',
-    video: 'https://www.youtube.com/watch?v=bzPYmBn-HeY&list=PL4oE8OvUySlyfGzQTu8kN9sPWWfcn_wSZ&index=7',
+    video: 'https://www.youtube.com/watch?v=6Az3AhKTzSw&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=8',
     children: {
       users: {full: 'Pengguna', icon: 'users'},
       references: {full: 'Referensi', icon: 'file-contract'}
@@ -65,7 +67,8 @@ menus = {
   },
   gizi: {
     full: 'Gizi', icon: 'utensils',
-    desc: 'Adalah menu khusus bagi petugas instalasi gizi untuk mengetahui rincian kebutuhan nutrisi bagi setiap pasien yang menginap di rumah sakit.'
+    desc: 'Adalah menu khusus bagi petugas instalasi gizi untuk mengetahui rincian kebutuhan nutrisi bagi setiap pasien yang menginap di rumah sakit.',
+    video: 'https://www.youtube.com/watch?v=-CagW5hkIIw&list=PL4oE8OvUySlyxLt2G6zROOCvrjeeXxNMR&index=9&t=3s'
   },
   cssd: {
     full: 'Laundry', icon: 'tshirt',
